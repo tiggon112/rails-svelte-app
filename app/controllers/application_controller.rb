8309f8ea7a51d87aca877ebd12c49003a9c23365
Funny Man <121869680+Stardev1127@users.noreply.github.com>
2023-10-31 03:49:12 -0400

    Fix Svelte controllers/application_controller.rb
    Co-authored-by: Tiggon <tiggon112@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      // this is update code
+      // It is just for authorization. Update needed at 10/31
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
